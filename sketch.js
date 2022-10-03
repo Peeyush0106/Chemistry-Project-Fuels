7389ee3c80af33f285c23d968e92cc8fc0c2297d
Peeyush0106 <peeyush.agarwal609@gmailcom>
2022-10-03 22:15:52 +0530

    Update

--- sketch.js
+++ sketch.js
@@ -1,56 +1,30 @@
 var currentQuote = -1;
-var timer = 10;
-
 var slide = 0;
-
 var bgsDone = ["no", "no", "no", "no", "no"];
-
-updateStartupTimer();
-
-async function updateStartupTimer() {
-    for (var i = 5; i > 0; i--) {
-        document.getElementById("start").innerText = i;
-        await new Promise(r => setTimeout(r, 1000));
-    }
-    blackOut();
-}
+var state = "greet"
 
 async function blackOut() {
+    state = "present";
     for (var i = 0; i < 1.07; i += 0.07) {
         await new Promise(r => setTimeout(r, 30));
         console.log("iterate")
         document.getElementById("blackout").style.opacity = i;
     }
     document.getElementById("greet").innerHTML = ""
-    document.getElementById("start").hidden = true;
     document.getElementById("greet-bg").hidden = true;
     document.getElementById("blackout").style.opacity = 0;
-    // startConfetti();
     document.getElementById("slides").hidden = false;
-    document.getElementById("timer").hidden = false;
-    showSlide();
-    updateTimer();
+    nextSlide();
 }
 
-async function updateTimer() {
-    document.getElementById("timer").innerText = timer;
-    await new Promise(r => setTimeout(r, 799))
-    if (timer > 0) {
-        timer -= 1;
-        document.getElementById("timer").innerText = timer;
-        updateTimer();
+async function nextSlide() {
+    if (document.getElementById("slide-" + slide)) {
+        document.getElementById("slide-" + slide).hidden = true
     }
-    else {
-        showSlide();
-        timer = 20;
-        updateTimer();
-    }
-}
-
-function showSlide() {
-    if (document.getElementById("slide-" + slide)) document.getElementById("slide-" + slide).hidden = true
     slide += 1;
-    if (!document.getElementById("slide-" + slide)) showThanks();
+    if (!document.getElementById("slide-" + slide)) {
+        showThanks();
+    }
     else {
         selectImage();
         document.getElementById("slide-" + slide).hidden = false;
@@ -71,7 +45,21 @@ async function selectImage() {
 }
 
 function showThanks() {
-    document.getElementById("timer").hidden = true;
     document.getElementById("slides").hidden = true;
+    document.getElementById("greet-bg").hidden = true;
+    document.getElementById("blackout").hidden = true;
+    document.getElementById("glass-over-slide").hidden = true;
     document.getElementById("thanks").hidden = false;
+}
+
+document.body.onkeypress = function (e) {
+    var k = e ? e.which : window.event.keyCode;
+    if (k == 32) {
+        if (state === "greet") {
+            blackOut()
+        }
+        else {
+            nextSlide();
+        }
+    }
 }
